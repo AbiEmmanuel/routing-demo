e6304d3e4d8367d1e0465ef4bbf9ace8c564a620
ldupas <louisdupas@net-c.com>
2020-10-26 17:58:26 +0100

    demo ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/gallery" component={Gallery} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div>
+            <h1>Je suis la page contact</h1>
+        </div>
+    )
+}
+
+export default Contact
